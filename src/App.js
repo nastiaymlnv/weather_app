47646fb4a22cd10ee9be73bba219386c57de5dfe
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-08-01 15:58:57 +0300

    add fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {weatherInfo: []};
+  }
+
+  apiKey = '81949147633542bcaba75017230108';
+  country = 'Vinnitsa';
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${this.apiKey}&q=${this.country}&days=1&aqi=no&alerts=no`);
+      this.setState({weatherInfo: response.data});
+      console.log(this.state.weatherInfo)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
