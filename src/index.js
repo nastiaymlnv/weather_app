c7726ab5a2d7df0b395f744e239033c9471840bf
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-08-01 12:03:26 +0300

    add theme provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 import './index.css';
 import App from './App';
 
+const darkTheme = createTheme({
+  palette: {
+    // mode: 'dark',
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
