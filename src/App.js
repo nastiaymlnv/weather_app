135d351b9553dd2970dc36171f3d15a5480958ce
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-08-03 17:25:59 +0300

    switch class component to functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -14,57 +14,55 @@ import './assets/reset.css';
 import './assets/fonts/fonts.css';
 import './App.css';
 
-export default class App extends Component {
+const App = () => {
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  const {location, current, forecast} = weatherInfo;
 
-  constructor(props) {
-    super(props);
-    this.state = {weatherInfo: []};
-  }  
+  const apiKey = '81949147633542bcaba75017230108';
+  const country = 'Vinnitsa';
+  const days = 7;
 
-  apiKey = '81949147633542bcaba75017230108';
-  country = 'Vinnitsa';
-  days = 7;
+  useEffect(() => {
+    fetchData();
+  })
 
-  async componentDidMount() {
+  const fetchData = async () => {
     try {
-      const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${this.apiKey}&q=${this.country}&days=${this.days}&aqi=no&alerts=no`);
-      this.setState({weatherInfo: response.data});
-      console.log(this.state.weatherInfo);
+      const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${country}&days=${days}&aqi=no&alerts=no`);
+      setWeatherInfo(response.data)
     } catch (error) {
       console.log(error);
     }
+  }  
+
+  if (!location) {
+    return <CircularProgress />
   }
 
-  render() {
-    const {location, current, forecast} = this.state.weatherInfo;
-
-    if (!location) {
-      return <CircularProgress />
-    }
-
-    return (
-      <Box sx={{pt: '29px', px: '100px', pb: '57px'}}>
-        <header className='header-container'>
-          <Typography variant='h1'>
-            {`${location.name}, ${location.country}`}
-          </Typography>
-          <SearchField />
-        </header>
-        <AllDayForecastCard currentDay={current} fewDaysForecast={forecast} />
-        <section className='few-days-weather-container'>
-          {
-            forecast.forecastday.slice(1).map(({date, day}) => {
-              return (
-                <FutureDayForecastCard 
-                  key={uuidv4()} 
-                  date={date} 
-                  dayInfo={day} 
-                />                
-              )
-            })
-          }
-        </section>
-      </Box>
-    )
-  }
+  return (
+    <Box sx={{pt: '29px', px: '100px', pb: '57px'}}>
+      <header className='header-container'>
+        <Typography variant='h1'>
+          {`${location.name}, ${location.country}`}
+        </Typography>
+        <SearchField />
+      </header>
+      <AllDayForecastCard currentDay={current} fewDaysForecast={forecast} />
+      <section className='few-days-weather-container'>
+        {
+          forecast.forecastday.slice(1).map(({date, day}) => {
+            return (
+              <FutureDayForecastCard 
+                key={uuidv4()} 
+                date={date} 
+                dayInfo={day} 
+              />                
+            )
+          })
+        }
+      </section>
+    </Box>
+  )
 }
+
+export default App;
