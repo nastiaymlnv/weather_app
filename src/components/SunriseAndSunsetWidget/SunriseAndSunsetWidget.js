508f72b83ff104e35821b85230a6f00511e8f625
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-08-08 17:06:07 +0300

    completed sunMove Widget logic

--- src/components/SunriseAndSunsetWidget/SunriseAndSunsetWidget.js
+++ src/components/SunriseAndSunsetWidget/SunriseAndSunsetWidget.js
@@ -16,9 +16,25 @@ const SunriseAndSunsetWidget = ({sunMove, classes}) => {
         var date = new Date("01/01/1970 " + timeString); 
         return date.getHours() + ':' + date.getMinutes(); 
     }
-
+    
+    const today = new Date();
+    const minutesFormat = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes();
+    const currentDate = ` ${today.getHours()}:${minutesFormat}`;
     const sunriseTimestamp = new Date(`01/01/1970 ${getTwentyFourHourTime(sunrise)}`).getTime();
     const sunsetTimestamp = new Date(`01/01/1970 ${getTwentyFourHourTime(sunset)}`).getTime();
+    const currentTimestamp = new Date(`01/01/1970 ${currentDate}`).getTime();
+
+    const generateAngleForCssSunMove = (sunriseTime, sunsetTime, currTime) => {
+        const percent = currTime * 100 / sunsetTime;
+        const angle = percent * 180 / 100;
+
+        return `${angle}deg`;
+    }
+    const generatePercentForCssSunMove = (sunriseTime, sunsetTime, currTime) => {
+        const percent = currTime * 100 / (sunsetTime - sunriseTime);
+
+        return `${percent - 5}%`;
+    }
 
     return (
         <article className='sunMove-container main-container-bg'>
@@ -26,11 +42,21 @@ const SunriseAndSunsetWidget = ({sunMove, classes}) => {
                 Sunrise and Sunset
             </Typography>
             <Box className={classes.SunMoveAnimationContainer} sx={{px: '15px'}}>
-                <div className='sunMove-trajectory'>
-                    <span className='sun-container'>
+                <div className='sunMove-trajectory-container' >
+                    <div 
+                        className="fill-bg"
+                        style={{'--currSunBeamPosition': generatePercentForCssSunMove(sunriseTimestamp, sunsetTimestamp, currentTimestamp)}}
+                    >
+                    </div>
+                </div>
+                <span>
+                    <span 
+                        className='sun-container' 
+                        style={{'--currSunPosition': generateAngleForCssSunMove(sunriseTimestamp, sunsetTimestamp, currentTimestamp)}}
+                    >
                         <img src={Sun} alt='Sun' className='sun' />
                     </span>
-                </div>
+                </span>                
             </Box>
             <Box sx={{
                 display: 'flex',
